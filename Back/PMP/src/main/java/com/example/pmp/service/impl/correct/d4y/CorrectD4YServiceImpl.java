3829b54c1eb56d8a07e06e38b0ee4411a4fdfc28
zhangdandandiy <3303850545@qq.com>
2024-02-27 16:46:42 +0800

    更改补正开始时间及结束时间

--- Back/PMP/src/main/java/com/example/pmp/service/impl/correct/d4y/CorrectD4YServiceImpl.java
+++ Back/PMP/src/main/java/com/example/pmp/service/impl/correct/d4y/CorrectD4YServiceImpl.java
@@ -64,7 +64,7 @@ public class CorrectD4YServiceImpl implements CorrectD4YService {
     /**
      * 设置开始时间
      */
-    String startTime = "2024-02-26 08:00:00.000";
+    String startTime = "2024-02-26 08:00:00.000" ;
     // String startTime = CorrectUtils.getCurrentCorrectStartTime();
     /**
      * 设置结束时间
--- Back/PMP/src/main/java/com/example/pmp/service/impl/correct/d9x/CorrectD9XServiceImpl.java
+++ Back/PMP/src/main/java/com/example/pmp/service/impl/correct/d9x/CorrectD9XServiceImpl.java
@@ -71,7 +71,7 @@ public class CorrectD9XServiceImpl implements CorrectD9XService {
     /**
      * 设置开始时间
      */
-    String startTime = "2024-02-26 08:00:00.000";
+    String startTime = "2024-02-26 08:00:00.000" ;
     // String startTime = CorrectUtils.getCurrentCorrectStartTime();
     /**
      * 设置结束时间
